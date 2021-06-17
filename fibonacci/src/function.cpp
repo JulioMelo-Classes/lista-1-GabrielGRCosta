2d7f9acb50d777d88c941567267955ff2de9233b
GabrielGRCosta <galvaogabriel890@gmail.com>
2021-06-17 19:18:01 +0000

    arquivos atualizados parte 5

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -2,8 +2,25 @@
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
-
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
+  int p1=0,p2=1,p=0;
+  int vet[n];
+  if(n==1){
+    return std::vector<unsigned int>{};
+  }
+  else if(n==2){
+    return {1,1};
+  }
+  for(int i=0;i<n;i++){
+    p = p1 + p2;
+    if(i>0){
+      p2=p;
+    }
+    p1 = p2;
+    vet[i] = p1;
+    if(i==1){
+      vet[i+1] = p2+1;
+    }
+    vet[i+1] = p2;  
+  }
     return std::vector<unsigned int>{};
 }
